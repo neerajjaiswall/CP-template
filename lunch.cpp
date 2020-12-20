cb6b153dc6ba67e79aa09a5f517bee118b7f9209
Neeraj Jaiswal <39668956+neerajjaiswall@users.noreply.github.com>
2020-12-20 17:12:48 +0530

    Update and rename cook2.cpp to lunch.cpp

---
+++ lunch.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace  std;
+
+typedef  long long ll;
+#define  int long long
+typedef  vector<int> vi;
+#define  fast ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define  pb push_back
+#define  fn for(int i=0;i<n;i++)
+#define  rep(i,a,b) for(ll i=a;i<b;i++)
+#define  all(x) x.begin(), x.end()
+#define  endl "\n"
+
+void solve()
+{
+	
+}
+
+int32_t main()
+{
+   	fast
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    int t; cin>>t; while(t--)
+    solve();
+}
